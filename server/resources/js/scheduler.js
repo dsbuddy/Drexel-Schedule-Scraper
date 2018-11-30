95bc6114a909d820bdf6ea5b98372bc08b1d2fa2
dsbuddy <des338@cs.drexel.edu>
2018-11-30 11:32:20 -0500

    Fixed overlap comparison of new data structure

--- server/resources/js/scheduler.js
+++ server/resources/js/scheduler.js
@@ -1,29 +1,54 @@
 
+/*
+//TESTS
+var class4 = {
+        'M': '12:00pm - 12:50pm',
+        'W': '12:00pm - 12:50pm',
+        'F': '12:00pm - 12:50pm'
+      };
+
+var class5 = {
+        'M': '11:00am - 11:20am',
+        'W': '11:00am - 11:20am',
+        'F': '11:00am - 11:20am'
+      };
+
+var class6 = {
+        'M': '11:30am - 11:50am',
+        'W': '11:30am - 12:20pm',
+        'F': '11:30am - 11:50am'
+      };
+
+console.log(overlap(class4, class5));
+console.log(overlap(class4, class6));
+console.log(overlap(class5, class6));
+*/
+
 function overlap(times1, times2) {
   var res = false;
-  for (var i=0; i<times1.length; i++) {
-    for (var j=0; j<times2.length; j++) {
-      res = res || overlapTimes(convert(times1[i]), convert(times2[j]));
+  for (var day1 in times1) {
+    for (var day2 in times2) {
+      res = res || overlapTimes(convert(day1, times1[day1]), convert(day2, times2[day2]));
     }
   }
   return res;
 }
 
+function convert(day, time) {
+  var timeList = time.split(" - ");
+  var boundaries = {
+    "startTime" : dayConvert(day, timeList[0]),
+    "endTime" : dayConvert(day, timeList[1])
+  };
+  return boundaries;
+}
+
+
+
 function overlapTimes(time1, time2) {
   return (Math.max(time1.startTime, time2.startTime) <= Math.min(time1.endTime, time2.endTime));
 }
 
-function convert(timeInput) {
-  for (var day in timeInput) {
-    var time = timeInput[day];
-    var timeList = time.split(" - ");
-    var boundaries = {
-      "startTime" : dayConvert(day, timeList[0]),
-      "endTime" : dayConvert(day, timeList[1])
-    };
-    return boundaries;
-  }
-}
 
 function dayConvert(day, time) {
   if (day == "M") return parseInt(convertTime(time)) + 0 * 24 * 60;
@@ -51,6 +76,9 @@ function isValidSchedule(sectionToAdd, schedule){
   for (otherClass of schedule){
     console.log(otherClass);
     console.log(sectionToAdd);
+	 console.log("TIMES ---------------");
+	 console.log(otherClass.times);
+	 console.log("END -----------------");
     if(overlap(otherClass.times, sectionToAdd.times)){
       return false;
     }
